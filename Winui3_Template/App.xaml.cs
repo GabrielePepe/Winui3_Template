c3714911dc2395427f96b450ce767fd3e9249f32
Gabriele pepe <Gabriele pepe@DESKTOP-6MTHQG3>
2025-05-11 14:46:00 +0200

    Final commit

--- Winui3_Template/App.xaml.cs
+++ Winui3_Template/App.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using DevWinUI;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Windowing;
+using WinRT.Interop;
+using ColorHelper = Microsoft.UI.ColorHelper;
 
 namespace Winui3_Template
 {
     public partial class App : Application
     {
         public ThemeService ThemeService { get; private set; }
+
+        private Window? m_window;
 
         public App()
         {
@@ -18,10 +24,42 @@ namespace Winui3_Template
 
             ThemeService = new ThemeService(m_window);
 
+            if (m_window.Content is FrameworkElement rootElement)
+            {
+                rootElement.ActualThemeChanged += RootElement_ActualThemeChanged;
+            }
+
+            UpdateCaptionButtonColors();
+
             m_window.Activate();
         }
 
-        private Window? m_window;
+        private void RootElement_ActualThemeChanged(FrameworkElement sender, object args)
+        {
+            UpdateCaptionButtonColors();
+        }
+
+        private void UpdateCaptionButtonColors()
+        {
+            var hWnd = WindowNative.GetWindowHandle(m_window);
+            var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+            var titleBar = appWindow.TitleBar;
+
+            if (m_window.Content is FrameworkElement fe)
+            {
+                var isDark = fe.ActualTheme == ElementTheme.Dark;
+
+                titleBar.ButtonForegroundColor = isDark ? Colors.White : Colors.Black;
+                titleBar.ButtonBackgroundColor = Colors.Transparent;
+                titleBar.ButtonHoverBackgroundColor = isDark
+                    ? ColorHelper.FromArgb(20, 255, 255, 255)
+                    : ColorHelper.FromArgb(20, 0, 0, 0);
+                titleBar.ButtonPressedBackgroundColor = isDark
+                    ? ColorHelper.FromArgb(40, 255, 255, 255)
+                    : ColorHelper.FromArgb(40, 0, 0, 0);
+            }
+        }
 
         public ThemeService GetThemeService()
         {
